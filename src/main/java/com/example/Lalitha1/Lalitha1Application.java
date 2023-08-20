c41d383fd7aa464647ddb8b1d02235e0fb026987
medari1048 <pushpakumar.medari@omniwyse.com>
2023-08-20 11:31:11 +0530

    my first commit sample program

---
+++ src/main/java/com/example/Lalitha1/Lalitha1Application.java
@@ -0,0 +1,13 @@
+package com.example.Lalitha1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lalitha1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lalitha1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Lalitha1/Lalitha1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lalitha1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lalitha1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
